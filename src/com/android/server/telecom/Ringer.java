af04f72f996ab85fcbe1ba559b6569c1e21a028c
Pranav Vashi <neobuddy89@gmail.com>
2020-03-23 06:07:18 +0530

    Fix few issues with Blink with call
    Change-Id: I81ae826915459bbc4aa619a692f1044cfa71fb28
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/server/telecom/Ringer.java
+++ src/com/android/server/telecom/Ringer.java
@@ -192,6 +192,8 @@ public class Ringer {
     private Handler mHandler = null;
     private int torchMode;
 
+    private boolean mBlinkActive;
+
     /**
      * Use lock different from the Telecom sync because ringing process is asynchronous outside that
      * lock
@@ -396,7 +398,8 @@ public class Ringer {
             shouldFlash = zenMode == Settings.Global.ZEN_MODE_OFF;
         }
 
-        if (shouldFlash) {
+        if (shouldFlash && !mBlinkActive) {
+            mBlinkActive = true;
             blinkFlashlight();
         }
 
@@ -495,6 +498,8 @@ public class Ringer {
     }
 
     public void startCallWaiting(Call call, String reason) {
+        mBlinkActive = false;
+
         if (mSystemSettingsUtil.isTheaterModeOn(mContext)) {
             return;
         }
@@ -536,8 +541,8 @@ public class Ringer {
                 mRingingCall = null;
             }
 
+            mBlinkActive = false;
             mRingtonePlayer.stop();
-            torchToggler.stop();
 
             // If we haven't started vibrating because we were waiting for the haptics info, cancel
             // it and don't vibrate at all.
@@ -691,7 +696,6 @@ public class Ringer {
 
     private class TorchToggler extends AsyncTask {
 
-        private boolean shouldStop = false;
         private CameraManager cameraManager;
         private int duration;
         private boolean hasFlash = true;
@@ -709,16 +713,12 @@ public class Ringer {
                     Settings.System.FLASHLIGHT_ON_CALL_RATE, 1, UserHandle.USER_CURRENT);
         }
 
-        void stop() {
-            shouldStop = true;
-        }
-
         @Override
         protected Object doInBackground(Object[] objects) {
             if (hasFlash) {
                 try {
                     String cameraId = cameraManager.getCameraIdList()[0];
-                    while (!shouldStop) {
+                    while (mBlinkActive) {
                         cameraManager.setTorchMode(cameraId, true);
                         Thread.sleep(duration);
 
